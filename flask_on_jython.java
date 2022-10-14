af38f9bc3f614078312952544e4489511d26fa2b
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2022-10-14 18:49:20 -0500

    Update flask_on_jython.java

--- flask_on_jython.java
+++ flask_on_jython.java
@@ -1,6 +1,8 @@
 import org.python.util.PythonInterpreter;
 
 public class flask_on_jython{
-
-
+  public static void main(String[] args) {
+    PythonInterpreter py = new PythonInterpreter();
+    py.execfile("flask_test.py");
+  }
 }
