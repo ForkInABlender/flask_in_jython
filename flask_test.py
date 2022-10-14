82d37c31d1508c3259039581f2e14cb5d056dc23
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2022-10-14 18:52:39 -0500

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -1,2 +1,7 @@
-import flask
-print(flask.__file__)
+from flask import Flask
+app = Flask(__name__)
+@app.route("/") 
+def index():
+    return "it worked"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
