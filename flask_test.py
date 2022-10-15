a7dad1a373666116b5a5bf1af6ca06a6361cdfe7
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2022-10-14 20:38:31 -0500

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+
+from java.lang import System
+
 app = Flask(__name__)
-@app.route("/") 
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == 'POST':
+        #print(request.data)
+        System.out.println("%s" % request.data)
     return "it worked"
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+
+app.run(host="127.0.0.1", port=5000)
